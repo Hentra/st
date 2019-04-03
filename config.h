2361b677423a30e1dec73899cab0285ea763bf4e
Henri Burau <henri.burau@web.de>
2019-04-04 00:18:41 +0200

    Change Modkey to Mod4

--- config.h
+++ config.h
@@ -193,7 +193,7 @@ static MouseShortcut mshortcuts[] = {
 };
 
 /* Internal keyboard shortcuts. */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 
 MouseKey mkeys[] = {
 	/* button               mask            function        argument */
